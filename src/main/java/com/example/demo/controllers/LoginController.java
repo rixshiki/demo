c94996d67f3e423bac48b130804d34b1693bc43e
VorakamolChinprasittichai <60010888@kmitl.ac.th>
2020-09-15 15:22:45 +0700

    login1

--- src/main/java/com/example/demo/controllers/LoginController.java
+++ src/main/java/com/example/demo/controllers/LoginController.java
@@ -1,42 +1,37 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import com.example.demo.entities.userprofile;
+import com.example.demo.repositories.UserProfileRepository;
 
 @Controller
+@SessionAttributes("user")
 public class LoginController {
 	
-	//@Autowired
-	//private UserProfileRepository userprofileRepo;
+	@Autowired
+	private UserProfileRepository userprofileRepo;
+	
+	@ModelAttribute("user")
+	public userprofile setUpUserForm() {
+		return new userprofile();
+	}
 	
 	@GetMapping("/")
 	public String login(Model model) {
 		return "login";
 	}
 	
-	/*
-	@GetMapping("/login")
-	 public String login(Model model) {
-		userprofile user = new userprofile();
-		user.setEmail("nice@mail.com");
-		user.setPassword("nice1234");
-		user.setName("Nice");
-		user.setBirthday(LocalDate.now());
-		user.setGender("female");
-		user.setAddress("xxxxxxxxxx");
-		user.setLine("@tijnuehc");
-		user.setTel("088888888");
-		user.setType("Seller");
-		user.setCoin(0);
-		userprofileRepo.save(user);
-	  return "login";
-	 }
-	@GetMapping("/")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
-	}*/
-
+	@GetMapping("/logout")
+	public String doLogout(SessionStatus sessionStatus) {
+		sessionStatus.setComplete();
+		return "redirect:/";
+	}
 }
