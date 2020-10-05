56ef6061a9f88927244e5fb44262a6a2705e46fc
VorakamolChinprasittichai <60010888@kmitl.ac.th>
2020-10-05 21:36:50 +0700

    edit and update

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.CreateFile;
@@ -43,5 +44,44 @@ public class UserController {					// about user and user tables
 		model.addAttribute("message", "You have already signed.");
 		model.addAttribute("user", new userprofile());
 		return "signup";
+	}
+	
+	@GetMapping("/edit")
+	public String editProfile(@SessionAttribute("user") userprofile user, Model model) {
+		System.out.println("Edit id:"+ user.getIdUser());
+		model.addAttribute("mem",userprofileRepo.getOne(user.getIdUser()));
+		return "changeinfo";
+	}
+	
+	@PostMapping("/updatemem")
+	public String updateMember(@ModelAttribute userprofile member
+			,@SessionAttribute("user") userprofile user
+			,@RequestParam("imageFile") MultipartFile file) throws IOException {
+		int id = member.getIdUser();
+		System.out.println("Update id:");
+		System.out.println(id);
+		userprofile mem = new userprofile();
+		mem = userprofileRepo.findById(id).get();
+		mem.setName(member.getName());
+		mem.setPassword(member.getPassword());
+		mem.setTel(member.getTel());
+		mem.setGender(member.getGender());
+		mem.setAddress(member.getAddress());
+		
+		user.setName(mem.getName());
+		user.setPassword(mem.getPassword());
+		user.setTel(mem.getTel());
+		user.setGender(mem.getGender());
+		user.setAddress(mem.getAddress());
+		
+		if(!file.isEmpty()) {
+			CreateFile bFile = new CreateFile();
+			String image = bFile.invertfile(file);
+			mem.setPhotoUser(image);
+			user.setPhotoUser(mem.getPhotoUser());
+		}
+		userprofileRepo.save(mem);
+		System.out.println("Update!!");
+		return "redirect:/chooseuser";
 	}
 }
