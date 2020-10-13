a7b2ec1092bbcdf9997d5fba7b5130c2d2a14777
VorakamolChinprasittichai <60010888@kmitl.ac.th>
2020-10-13 15:29:13 +0700

    category finished

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,10 +24,12 @@ public class ProductController {
 	@Autowired
 	private CategoryRepository categoryRepo;
 	
-	@GetMapping("/editproduct")
-	public String editproduct(Model model) {
+	@GetMapping("/editproduct/{idcat}")
+	public String editproduct(@PathVariable("idcat") String idcat
+			,Model model) {
 		List<category> catlist = new ArrayList<category>();
 		catlist = categoryRepo.findAll();
+		model.addAttribute("cat", categoryRepo.getOne(idcat));
 		model.addAttribute("catlist", catlist);
 		return "editproduct";
 	}
@@ -54,7 +57,24 @@ public class ProductController {
 			category.setPhotoCat(image);
 		}
 		categoryRepo.save(category);
-		return "redirect:/editproduct";
+		return "redirect:/editproduct/1";
+	}
+	
+	@PostMapping("/updatecat")
+	public String updateMember(@ModelAttribute category category
+			,@RequestParam("imageFile") MultipartFile file) throws IOException{
+		System.out.println(category.getNameCat());
+		category cat = new category();
+		cat = categoryRepo.findById(category.getIdCategory()).get();
+		cat.setDetailCat(category.getDetailCat());
+		if(!file.isEmpty()) {
+			CreateFile bFile = new CreateFile();
+			String image = bFile.invertfile(file);
+			cat.setPhotoCat(image);
+		}
+		categoryRepo.save(cat);
+		System.out.println("Category Update!!");
+		return "redirect:/editproduct/1";
 	}
 	
 }
--- src/main/java/com/example/demo/entities/category.java
+++ src/main/java/com/example/demo/entities/category.java
@@ -1,6 +1,8 @@
 package com.example.demo.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -8,11 +10,18 @@ import javax.persistence.Table;
 @Table(name = "category")
 public class category {
 	
-	@Id
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String idCategory;
 	private String nameCat;
 	private String detailCat;
 	private String photoCat;
 	
+	public String getIdCategory() {
+		return idCategory;
+	}
+	public void setIdCategory(String idCategory) {
+		this.idCategory = idCategory;
+	}
 	public String getNameCat() {
 		return nameCat;
 	}
@@ -36,6 +45,7 @@ public class category {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((detailCat == null) ? 0 : detailCat.hashCode());
+		result = prime * result + ((idCategory == null) ? 0 : idCategory.hashCode());
 		result = prime * result + ((nameCat == null) ? 0 : nameCat.hashCode());
 		result = prime * result + ((photoCat == null) ? 0 : photoCat.hashCode());
 		return result;
@@ -54,6 +64,11 @@ public class category {
 				return false;
 		} else if (!detailCat.equals(other.detailCat))
 			return false;
+		if (idCategory == null) {
+			if (other.idCategory != null)
+				return false;
+		} else if (!idCategory.equals(other.idCategory))
+			return false;
 		if (nameCat == null) {
 			if (other.nameCat != null)
 				return false;
@@ -66,6 +81,5 @@ public class category {
 			return false;
 		return true;
 	}
-	
 	
 }
