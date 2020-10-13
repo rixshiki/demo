fa69706f3d1913f33f1b4686c00b47d379e79b6b
VorakamolChinprasittichai <60010888@kmitl.ac.th>
2020-10-13 13:39:43 +0700

    category

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -1,18 +1,40 @@
 package com.example.demo.controllers;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.example.demo.CreateFile;
 import com.example.demo.entities.category;
+import com.example.demo.repositories.CategoryRepository;
 
 @Controller
 public class ProductController {
-
+	@Autowired
+	private CategoryRepository categoryRepo;
+	
 	@GetMapping("/editproduct")
-	public String editproduct() {
-		
+	public String editproduct(Model model) {
+		List<category> catlist = new ArrayList<category>();
+		catlist = categoryRepo.findAll();
+		model.addAttribute("catlist", catlist);
 		return "editproduct";
+	}
+	
+	@GetMapping("/addoneproduct")
+	public String addoneproduct() {
+		
+		return "addoneproduct";
 	}
 	
 	@GetMapping("/addcategory")
@@ -21,9 +43,18 @@ public class ProductController {
 		return "addcategory";
 	}
 	
-	@GetMapping("/addoneproduct")
-	public String addoneproduct() {
-		
-		return "addoneproduct";
+	@PostMapping("/savecat")
+	public String saveCategory(@ModelAttribute category category
+			, BindingResult errors
+			, Model model
+			,@RequestParam("imageFile") MultipartFile file) throws IOException {
+		if(!file.isEmpty()) {
+			CreateFile bFile = new CreateFile();
+			String image = bFile.invertfile(file);
+			category.setPhotoCat(image);
+		}
+		categoryRepo.save(category);
+		return "redirect:/editproduct";
 	}
+	
 }
