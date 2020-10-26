940972e9e9019e848e4d456f7d08d06ef2d3e566
VorakamolChinprasittichai <60010888@kmitl.ac.th>
2020-10-26 23:11:03 +0700

    update checking page

--- src/main/java/com/example/demo/controllers/OrderController.java
+++ src/main/java/com/example/demo/controllers/OrderController.java
@@ -1,9 +1,14 @@
 package com.example.demo.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.entities.userorder;
 import com.example.demo.repositories.OrderDetailRepository;
 import com.example.demo.repositories.PaymentRepository;
 import com.example.demo.repositories.UserOrderRepository;
@@ -20,20 +25,24 @@ public class OrderController {
 	private PaymentRepository paymentRepo;
 	
 	@GetMapping("/checking")
-	public String findchecking() {
-		
+	public String findchecking(Model model) {
+		List<userorder> userorders = new ArrayList<userorder>();
+		userorders = userorderRepo.getByStatus("checking");
+		model.addAttribute("userorders", userorders);
 		return "transfercheck";
 	}
 	
 	@GetMapping("/tracking")
 public String findtracking() {
-		
+		//List<userorder> userorders = new ArrayList<userorder>();
+		//userorders = userorderRepo.getByStatus("tracking");
 		return "transfertrack";
 	}
 	
 	@GetMapping("/complete")
 public String findcomplete() {
-		
+		//List<userorder> userorders = new ArrayList<userorder>();
+		//userorders = userorderRepo.getByStatus("complete");
 		return "transfercomplete";
 	}
 }
--- src/main/java/com/example/demo/repositories/UserOrderRepository.java
+++ src/main/java/com/example/demo/repositories/UserOrderRepository.java
@@ -1,9 +1,16 @@
 package com.example.demo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.entities.userorder;
 
 public interface UserOrderRepository extends JpaRepository<userorder, Integer> {
+	
+	@Query("from userorder o where o.status = :status order by o.payTime asc")
+	List<userorder> getByStatus(@Param("status")String status);
 
 }
