23047ae61b391e622ee6e9ff8ef002e5560d3020
VorakamolChinprasittichai <60010888@kmitl.ac.th>
2020-11-03 23:43:31 +0700

    delivery

--- src/main/java/com/example/demo/controllers/DeliveryController.java
+++ src/main/java/com/example/demo/controllers/DeliveryController.java
@@ -1,17 +1,46 @@
 package com.example.demo.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entities.delivery;
+import com.example.demo.repositories.DeliveryRepository;
 
 @Controller
 public class DeliveryController {
-
+	
+	@Autowired
+	private DeliveryRepository deliveryRepo;
+	
 	@GetMapping("/editdelivery")
 	public String editdelivery(Model model) {
-		model.addAttribute("dalivery", new delivery());
-		return "editdelivery";
+		List<delivery> deliveryList = new ArrayList<delivery>();
+		deliveryList = deliveryRepo.findAll();
+		model.addAttribute("delivery", new delivery());
+		model.addAttribute("deliveryList", deliveryList);
+		return "adddeliveryprice";
 	}
+	
+	@PostMapping("/savedelivery")
+	public String savedelivery(@ModelAttribute delivery delivery
+			,@RequestParam("textname") String textname
+			,@RequestParam("selectname") String selectname
+			,@RequestParam("hidden") String hidden) {
+		if(hidden.equalsIgnoreCase("text")) {
+			delivery.setNameDelivery(textname);
+		}else if(hidden.equalsIgnoreCase("select")) {
+			delivery.setNameDelivery(selectname);
+		}
+		deliveryRepo.save(delivery);
+		return "redirect:/editaccount";
+	}
+	
 }
